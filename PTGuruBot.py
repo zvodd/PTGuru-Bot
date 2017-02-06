6c6c5b8629dca389d98594934bedb1663af7b992
thescriptkid <secondclass325@gmail.com>
2017-02-06 14:10:53 -0600

    added regex library re

--- PTGuruBot.py
+++ PTGuruBot.py
@@ -2,33 +2,48 @@
 import discord
 import logging
 import asyncio
-import aiohttp
 import feedparser
 import time
+import re
 
-logging.basicConfig(filename="log.txt", level=logging.INFO, format="%(asctime)s - %(levelname)s [+]%(message)s")
+logging.basicConfig(filename="gurubot-log.txt", level=logging.INFO, format="%(asctime)s - %(levelname)s [+]%(message)s")
 log = logging.getLogger()
 
 client = discord.Client()
-author = "```Learning Penetration Testing\n\nThe Penetration Testing Community```"
-Help = "```!!author\n!!joinredteam5```"
+
+author = "**Learning Penetration Testing\n\nThe Penetration Testing Community**"
+Help = "**!!author\n!!joinredteam5**"
+
+newslist = open("newslist.txt", "r")
 cve_feed = "http://bit.ly/2k97VtF"
-news_channel = "275289702985367564"
-vulnerability_channel = "277950292807516161"
-newslist = open("newslist.txt", "r").read()
+ctf_upcoming = "https://ctftime.org/event/list/upcoming/rss/"
+
+news_channel = "278244337752735744"
+vulnerability_channel = "278244156759867392"
+ctf_channel = "274925767924776960"
+
+cve_feeder = feedparser.parse(cve_feed)
+ctf_feeder = feedparser.parse(ctf_upcoming)
+
 current_date = time.strftime("%m-%d-%Y")
+
 
 @client.event
 async def on_ready():
     log.info("Initialzation Complete..")
-    log.info("Guru Bot Ready.")
+    log.info("Guru! Guru!.")
+    await post_upcoming_ctfs()
+    await post_vulnerabilities()
 
 async def post_feeds():
-    await client.send_message(client.get_channel(news_channel), "```Test News Feed```")
+    await client.send_message(client.get_channel(news_channel), "**Test News Feed**")
 
-async def post_exploits():
-    async with aiohttp.get(cve_feed) as link:
-        await client.send_message(client.get_channel(vulnerability_channel), "```Vulnerability of The Day```")
+async def post_vulnerabilities():
+    await client.send_message(client.get_channel(vulnerability_channel), "**New Posted Vulnerability!!{}\n{}\n{}**".format(cve_feeder.entries[0].title, cve_feeder.entries[0].summary, cve_feeder.entries[0].link))
+
+async def post_upcoming_ctfs():
+    await client.send_message(client.get_channel(ctf_channel), "**Newly Posted CTF!!\n\n{}\n\n{}**".format(ctf_feeder.entries[0].title, re.sub("<.*?>", "", ctf_feeder.entries[0].summary)))
+
 
 @client.event
 async def on_message(message):
