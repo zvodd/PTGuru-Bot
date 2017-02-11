5692282c82752211192443586e7b10ee6498f4eb
zvodd <zv.odd.101@gmail.com>
2017-02-11 13:12:11 +1100

    example json feed config

--- PTGuruBot/ptg_configure.py
+++ PTGuruBot/ptg_configure.py
@@ -11,18 +11,6 @@ logging.basicConfig(filename="gurubot-log.txt", level=logging.INFO, format="%(as
 config = configparser.RawConfigParser()
 config.read("settings.cfg")
 
-# News Feeds
-CTF_FEED_URL = config.get('feeds', 'ctf_feed_url')
-CVE_FEED_URL = config.get('feeds', 'cve_feed_url')
-## hard coded
-with open("assets/newslist.txt", "r") as fh:
-    NEWS_FEED_URLS = [line for line in fh]
-
-# Channel IDs
-NEWS_CHANNEL = config.get('channels', 'news_channel')
-CVE_CHANNEL = config.get('channels', 'vulnerability_channel')
-CTF_CHANNEL = config.get('channels', 'ctf_channel')
-
 # New Post Limit, per feed.
 MAX_NEW_POSTS = int(config.get("limits", "max_new_posts"))
 
